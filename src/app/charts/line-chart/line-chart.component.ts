8ba636b9d2896d5aed62cbb791263fae04246cce
Mads F Engelbreth <mads.engelbreth@gmail.com>
2019-06-30 14:51:14 +0200

    added line Chart - Alligning needs to be fixed - it's clipping out of box-card

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {LINE_CHART_COLORS } from '../../shared/chart.colors';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [5,6,3,3,4,6,4], label: 'Mads point'},
+  { data: [3,6,9,4,2,3,6], label: 'Flow point'}
+];
+
+const LINE_CHART_LABELS: string[] = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +16,15 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true
+  };
+  lineChartLegend: true;
+  lineChartType = 'line';
+  lineChartColors = LINE_CHART_COLORS;
 
   ngOnInit() {
   }
